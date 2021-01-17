ebc5fd549555afd68e3413814cc48845aae3b356
wlkitedus014 <77576998+wlkitedus014@users.noreply.github.com>
2021-01-18 04:27:00 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
-  <p> 최지연 </p>
+<div>
+    <p> 최지연 최고!</p>
+<Button Primary> hi </Button>
+<Progress percent = {33} indicating/>
+</div>
   );
 }
 
